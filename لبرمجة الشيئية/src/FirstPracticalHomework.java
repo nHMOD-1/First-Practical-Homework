fbf810e6ddcf3447d4f8028ab68532c22de4d8ee
kewpf <kewpf@DESKTOP-F8DVBEC>
2021-02-04 12:15:19 +0300

    First Commit

---
+++ لبرمجة الشيئية/src/FirstPracticalHomework.java
@@ -0,0 +1,13 @@
+// This program is written by Mohammed Ballhareth
+// Date:4-4-2021
+// Course Name:Object-oriented programming
+
+  public class FirstPracticalHomework {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+    
+	System.out.println("Mohammed Ballhareth");
+    }
+
+}
